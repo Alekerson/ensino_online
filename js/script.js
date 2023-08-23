f1e132489716482377251de77bfbae0f8a3ca017
Alekerson <Alekersonm@gmail.com>
2023-08-22 21:00:52 -0300

    Inicado Cadastro

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let lista = document.querySelector("#listas")
 
 async function carregarDados(){
-    const url ="https://escola.free.beeceptor.com/all"
+    const url ="https://run.mocky.io/v3/0cb11e4d-798b-47aa-9b71-03df87cc814d"
 
     const resultado = await fetch(url)
 
@@ -16,14 +16,24 @@ async function carregarDados(){
         const linha3 = document.createElement("button")
         const linha4 = document.createElement("button")
         const linha5 = document.createElement("button")
+        
+        const linha6 = document.createElement("span")
+        const linha7 = document.createElement("span")
+        const linha8 = document.createElement("span")
+        const linha9 = document.createElement("span")
 
 
         divizera.classList.add("col-lg-4","col-md-6", "list-group")
-        linha1.classList.add("list-group-item", "list-group-item-action active")
+        linha1.classList.add("list-group-item", "list-group-item-action", "active", "fw-bold")
         linha2.classList.add("list-group-item", "list-group-item-action")
         linha3.classList.add("list-group-item", "list-group-item-action")
         linha4.classList.add("list-group-item", "list-group-item-action")
         linha5.classList.add("list-group-item", "list-group-item-action")
+
+        linha6.classList.add("fw-bold")
+        linha7.classList.add("fw-bold")
+        linha8.classList.add("fw-bold")
+        linha9.classList.add("fw-bold")
 
         linha1.textContent = itens.nome_escola
         linha2.textContent = itens.descrição_resumida
@@ -31,11 +41,22 @@ async function carregarDados(){
         linha4.textContent = itens.telefone
         linha5.textContent = itens.email
 
+        linha6.textContent ="Descrição: "
+        linha7.textContent = "Endereço: "
+        linha8.textContent = "Telefone: "
+        linha9.textContent = "Email: "
+
         divizera.appendChild(linha1)
         divizera.appendChild(linha2)
         divizera.appendChild(linha3)
         divizera.appendChild(linha4)
         divizera.appendChild(linha5)
+
+        linha2.prepend(linha6)
+        linha3.prepend(linha7)
+        linha4.prepend(linha8)
+        linha5.prepend(linha9)
+      
 
         lista.appendChild(divizera)
     }
