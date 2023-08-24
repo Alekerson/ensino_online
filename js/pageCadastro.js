83da856c3716c791260dd2c3f083f4a50ecb634d
Luciano <luciano_jet@hotmail.com>
2023-08-24 17:31:13 -0300

    Finalizado

--- js/pageCadastro.js
+++ js/pageCadastro.js
@@ -17,110 +17,47 @@
       }, false)
     })
   })()
-// let nome = document.querySelector("#nome")
-// let sobrenome = document.querySelector("#sobrenome")
-// let idade = document.querySelector("#idade")
-// let rua = document.querySelector("#rua")
-// let numero = document.querySelector("#numero")
-// let bairro = document.querySelector("#bairro")
+
 let tel = document.querySelector("#tel")
-// let genero = document.querySelector("#genero")
-// let data = document.querySelector("#data")
 
-// // EVENTO DE NOME
-
-// // EVENTO IDADE
-// sobrenome.addEventListener("blur", function(){
-//     if(sobrenome.value == ""){
-//         sobrenome.style.outline = "1px solid red"}
-//     else{
-//         sobrenome.style.outline = "1px solid green"
-//     }
-// })
-// rua.addEventListener("blur", function(){
-//     if(rua.value == ""){
-//         rua.style.outline = "1px solid red"}
-//     else{
-//         rua.style.outline = "1px solid green"
-//     }
-// })
-
-// bairro.addEventListener("blur", function(){
-//     if(bairro.value == ""){
-//         bairro.style.outline = "1px solid red"}
-//     else{
-//         bairro.style.outline = "1px solid green"
-//     }
-// })
-
-// tel.addEventListener("blur", function(){
-//     if(tel.value == ""){
-//         tel.style.outline = "1px solid red"}
-//     else{
-//         tel.style.outline = "1px solid green"
-//     }
-// })
-
-
-// let cpf = document.querySelector("#cpf")
-// let cartao = document.querySelector("#cartao")
-// let aux = ""
-
-// cpf.addEventListener('keyup', function(event){
-//      if(event.keyCode != 32 && event.keyCode != 46){
-//         console.log(cpf.value.length)
-//         let i = cpf.value.length
-//         aux = cpf.value
-//         if (i == 3 || i == 7){
-//             cpf.value = aux + "."
-//         }
-//         if (i == 11){
-//             cpf.value = aux + "-"
-//         }
-    
-//     }})
-
-
-// cartao.addEventListener('keyup', function(event){
-//     if (event.keyCode != 32 && event.keyCode != 46){
-//         let i = cartao.value.length
-//         aux = cartao.value
-//         if (i == 4 || i == 9 || i == 14){
-//             cartao.value = aux + " "
-//         }
-//     }
-// })
 
 tel.addEventListener('keypress', function(event){
-    if (event.keyCode != 32 && event.keyCode != 46){
-        let i = tel.value.length
-        aux = tel.value
-        console.log(tel.value.length)
-        if(i == 0)
-            tel.value = aux + "("
-        if(i == 3)
-            tel.value = aux + ")"
-    }
+  if (event.keyCode != 32 && event.keyCode != 46){
+    let i = tel.value.length
+    aux = tel.value
+    console.log(tel.value.length)
+    if(i == 0)
+    tel.value = aux + "("
+    if(i == 3)
+    tel.value = aux + ")"
+  }
 })
 
-// let infoPessoal = document.querySelector("#infoPessoal")
-// let infoCartao = document.querySelector("#infoCartao")
-// let infoFinalizando = document.querySelector("#infoFinalizando")
-// let btnPessoal = document.querySelector("#prox")
-// let btnCartao = document.querySelector("#next")
 
-// infoCartao.classList.add("d-none")
-// infoFinalizando.classList.add("d-none")
+let cpf = document.querySelector("#cpf")
 
-// // btnPessoal.addEventListener('click', function(){
-// //     infoPessoal.classList.add("d-none")
-// //     infoCartao.classList.replace("d-none", "d-block")
+cpf.addEventListener('keyup', function(event){
+     if(event.keyCode != 32 && event.keyCode != 46){
+        console.log(cpf.value.length)
+        let i = cpf.value.length
+        aux = cpf.value
+        if (i == 3 || i == 7){
+            cpf.value = aux + "."
+        }
+        if (i == 11){
+            cpf.value = aux + "-"
+        }
+    
+    }})
 
-// // })
+let cartao = document.querySelector("#cartao")
 
-// // btnCartao.addEventListener('click', function(){
-// //     infoCartao.classList.add("d-none")
-// //     infoFinalizando.classList.replace("d-none", "d-block")
-// // })
-
-// Example starter JavaScript for disabling form submissions if there are invalid fields
+cartao.addEventListener('keyup', function(event){
+    if (event.keyCode != 32 && event.keyCode != 46){
+        let i = cartao.value.length
+        aux = cartao.value
+        if (i == 4 || i == 9 || i == 14){
+            cartao.value = aux + " "
+        }
+    }
+})
